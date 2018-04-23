fbb1e3322e31d1e92d2cbbe7813ca739ef3edc8a
Luis Jorge <gevolutionxalapa@gmail.com>
2018-04-23 12:05:33 -0500

    first commit

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
-// server.js
+var express = require('express')
+var http = require('http')
+var app = express()
 
-// BASE SETUP
-// =============================================================================
+var users = ['oscar', 'juan', 'marcos']
 
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
+app.get('/users', (req, res) => {
+  res.send(users)
+})
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.status(200).send("Welcome to API REST")
+})
 
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+http.createServer(app).listen(8001, () => {
+  console.log('Server started at http://localhost:8001 aqui mero');
 });
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
